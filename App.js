1165c017c2af99d28b45e4216229aeba42a15934
qw6375@gmail.com <timek70@dalcomsoft.com>
2022-06-09 16:00:04 +0900

    ScrollView 컴포넌트를 이용하여 마크업 수정

--- App.js
+++ App.js
@@ -1,5 +1,9 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native"
+import { View, Text, Dimensions, StyleSheet, ScrollView } from "react-native"
+
+// 화면 크기를 받아옴
+// ES6. width값을 가져온 후 이름을 SCREEN_WIDTH로 바꿈
+const { width: SCREEN_WIDTH } = Dimensions.get("window");
 
 export default function App() {
   return (
@@ -7,12 +11,41 @@ export default function App() {
       <View style={styles.city}>
         <Text style={styles.cityName}>Seoul</Text>
       </View>
-      <View style={styles.weather}>
+      {/* - ScrollView is native scroll view provided by OS(customizable)
+          - horizontal props를 추가하여 가로 스크롤이 되게끔
+          - ScrollView의 스타일을 주고싶다면 style props를 사용하면 안된다
+          - 대신 contentContainerStyle을 사용해야 한다
+            - 스크롤이 안되는데, ScrollView 컴포넌트에는 flex를 줄 필요가 없기때문(스크롤이 화면보다 커야하기에)
+          - 가로 스크롤을 했을 때, 슬라이더가 낭창(?)거리지 않게 딱딱 끊어지며 한 화면씩 보여줘야 한다.
+            - ScrollView컴포넌트에 pagingEnabled props 사용(boolean, default: false)
+            - pagingEnabled를 추가했더니 디스플레이 최 하단에 indicator가 생겼다.
+              - showsHorizontalScrollIndicator props때문(boolean, default: true)
+              - indicatorStyle props를 사용하면 indicator를 스타일링 할 수 있다(IOS only)
+            
+      */}
+      <ScrollView 
+        pagingEnabled
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={styles.weather}
+      >
         <View style={styles.day}>
           <Text style={styles.temp}>27</Text>
           <Text style={styles.description}>Sunny</Text>
         </View>
-      </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+      </ScrollView>
     </View>
   );
 }
@@ -34,10 +67,12 @@ const styles = StyleSheet.create({
     fontWeight: "500",
   },
   weather: {
-    flex: 3,
+    
   },
   day: {
-    flex: 1,
+    // width를 고정값으로 입력하면 AOS/IOS별로, 화면 크기별로 레이아웃이 변경되니까
+    // 이를 막기 위해 RN의 Dimensions API를 import하여 width에 기기 가로크기를 적용
+    width: SCREEN_WIDTH,
     alignItems: "center",
   },
   temp: {
